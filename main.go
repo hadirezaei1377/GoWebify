7dcb7a41c8eab3b055cead669c3e30c8944ec293
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-06-15 15:03:02 +0330

    hello world in webserver

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello  world!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprint(w, "Hello world!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
+
 }
