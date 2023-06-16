e789898ab09a305cec5974832b14ab46220a3a6f
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-06-16 11:35:09 +0330

    refacotring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/hadirezaei1377/gowebify/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
