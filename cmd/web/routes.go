def27c08816a3c00439f4c5b7ede695dd6cfa736
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-06-22 14:41:11 +0330

    test for aploading pictures

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,11 @@ func routes(app *config.AppConfig) http.Handler { // return a route for applicat
 	mux.Use(SessionLoad)             // adds custom middleware to the chi router that loads the session data for each HTTP request
 	mux.Get("/", handlers.Repo.Home) // Route definitions
 	mux.Get("/about", handlers.Repo.About)
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
+
+// . means in root of folder
+// * means any thing in folder
