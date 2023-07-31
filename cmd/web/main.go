04913ba8060e9c651e065367a689036e230bffdd
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-07-31 15:24:07 +0330

    write test for main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,6 +22,63 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+		return err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+	return nil
+}
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main function
+func main() {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 
---
+++ cmd/web/routes_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/tsawler/bookings-app/internal/config"
+)
+
+func TestRoutes(t *testing.T) {
+	var app config.AppConfig
+
+	mux := routes(&app)
+
+	switch v := mux.(type) {
+	case *chi.Mux:
+		// do nothing
+	default:
+		t.Error(fmt.Sprintf("type is not *chi.Mux, but is %T", v))
+	}
+}
